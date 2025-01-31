3dd96da8d68b3c74ce44419c1270fcdbd81492d5
JHD <jan_helge.doersam@tu-darmstadt.de>
2025-01-31 18:23:02 +0100

    Changed how we expose the api

--- transientbvd/__init__.py
+++ transientbvd/__init__.py
@@ -1,19 +1,62 @@
-# Import functions and classes from individual files
-from .open import open_potential, tau_decay, two_tau_decay, optimum_resistance, print_open_potential
-from .transducer import predefined_transducer, predefined_transducers, Transducer, load_transducers
-from .utils import roots, resonance_frequency
+# __init__.py
+"""
+TransientBVD: A library for analyzing transient behavior in the Butterworth-Van Dyke (BVD) model.
+"""
 
-# Define the public API
+# open.py
+from .open import (
+    open_potential,
+    print_open_potential,
+    open_tau,
+    open_two_tau,
+    optimum_resistance,
+    open_current,
+)
+
+# closed.py
+from .closed import (
+    closed_current,
+    switching_time,
+    closed_4tau,
+    closed_potential,
+    print_closed_potential,
+)
+
+# transducer.py
+from .transducer import (
+    Transducer,
+    load_transducers,
+    predefined_transducer,
+    predefined_transducers,
+)
+
+# utils.py
+from .utils import (
+    resonance_frequency,
+    roots,
+)
+
+# Define an __all__ so that `from transientbvd import *` will only import these symbols
 __all__ = [
-    "print_open_potential",
+    # From open.py
     "open_potential",
-    "tau_decay",
-    "two_tau_decay",
+    "print_open_potential",
+    "open_tau",
+    "open_two_tau",
     "optimum_resistance",
-    "predefined_transducer",
-    "predefined_transducers",
+    "open_current",
+    # From closed.py
+    "closed_current",
+    "switching_time",
+    "closed_4tau",
+    "closed_potential",
+    "print_closed_potential",
+    # From transducer.py
     "Transducer",
     "load_transducers",
-    "roots",
+    "predefined_transducer",
+    "predefined_transducers",
+    # From utils.py
     "resonance_frequency",
+    "roots",
 ]
