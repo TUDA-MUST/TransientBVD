84ab1a8a3da45dfc9420169ab7d8781a3d61bb47
JHD <jan_helge.doersam@tu-darmstadt.de>
2025-01-31 18:36:19 +0100

    Added first .setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="TransientBVD",
+    version="0.1.0",
+    description="Transient response analysis for BVD model",
+    author="Jan Helge Dörsam",
+    packages=["transientbvd"],
+)
